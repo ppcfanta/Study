8c6615b69b1a5962ad11c293106c5b93182f9dca
Ivan Malakhov <artpromstroi@gmail.com>
2021-02-16 23:36:03 +0300

    Lesson 2 homework

---
+++ Lesson2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minTemp, maxTemp;
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
