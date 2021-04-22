d0f73028e1fffc70572dece0de9514c342b25ff9
Ivan Malakhov <artpromstroi@gmail.com>
2021-04-22 18:12:12 +0300

    lesson2 homework added

---
+++ quarter 1/Part 3. Algorithms and data structures/Lesson2/NodeList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Lesson2
+{
+    public class NodeList : ILinkedList
+    {
+        private List<Node> nodeList;
+
+        public NodeList()
+        {
+            nodeList = new List<Node>();
+        }
+
+        public void PrintNodeList()
+        {
+            var currNode = GetFirst();
+            while (currNode != null)
+            {
+                Console.Write(currNode.Value+"\t");
+                currNode = currNode.NextNode;
+            }
+        }
+        public Node GetFirst()
+        {
+            foreach (var node in nodeList)
+            {
+                if (node.PrevNode == null)      // ищем первый элемент двусвязанного списка
+                    return node;
+            }
+            return null;
+        }
+        public Node GetLast()
+        {
+            foreach (var node in nodeList)
+            {
+                if (node.NextNode == null)      // ищем последний элемент двусвязанного списка
+                    return node;
+            }
+            return null;
+        }
+
+        public Node GetNodeByIndex(int index)
+        {
+            if (index > nodeList.Count - 1)
+                return null;
+            return nodeList[index];
+        }
+
+        public void AddNode(int value)
+        {
+            var prev = GetLast();
+            var node = new Node(value, prev, null);
+            if (prev!=null)
+                prev.NextNode = node;
+            nodeList.Add(node);
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            var newNode = new Node(value, node, node.NextNode);
+            node.NextNode = newNode;
+            nodeList.Add(newNode);
+            if (node.NextNode != null)
+                node.NextNode.PrevNode = newNode;
+
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            foreach (var item in nodeList)
+            {
+                if (item.Value==searchValue)
+                    return item;
+            }
+            return null;        // элемент не найден
+        }
+
+        public int GetCount()
+        {
+            return nodeList.Count;
+        }
+
+        public void RemoveNode(int index)
+        {
+            if (index > nodeList.Count - 1)
+                return;     // если пытаемся удалить элемента по индексу, выходящему за рамки - ничего не делаем.
+
+            nodeList[index - 1].NextNode = nodeList[index].NextNode;
+            nodeList.RemoveAt(index);
+        }
+
+        public void RemoveNode(Node node)
+        {
+            Node prev, next;
+            prev = node.PrevNode;
+            next = node.NextNode;
+            prev.NextNode = next;
+            nodeList.Remove(node);
+        }
+    }
+
+}
---
+++ quarter 1/Part 3. Algorithms and data structures/Lesson2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Channels;
+
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //****************** Задание 1 ***********************
+            Console.WriteLine("Задание 1. Реализовать двусвязанный список.");
+            var nodes = new NodeList();
+            nodes.AddNode(45);
+            nodes.AddNode(5);
+            nodes.AddNode(12);
+            nodes.AddNode(88);
+            //Console.WriteLine(nodes.FindNode(12).Value);
+            Console.WriteLine("Изначальный список:");
+            nodes.PrintNodeList();
+
+            Console.WriteLine("\nДобавляем элемент(77) после 12 , выводим список:");
+            nodes.AddNodeAfter(nodes.GetNodeByIndex(2), 77);
+            nodes.PrintNodeList();
+
+            Console.WriteLine("\nДобавляем новый элемент(значение=99) в конец списка, выводим список:");
+            nodes.AddNode(99);
+            nodes.PrintNodeList();
+
+            Console.WriteLine("\nУдаляем элемент с порядковым номером 2, выводим список:");
+            nodes.RemoveNode(1);
+            nodes.PrintNodeList();
+
+            //****************** Задание 2 ***********************
+            Console.WriteLine("\n\nЗадание 2. Реализовать бинарный поиск.");
+            var myArray = new IntArray();
+            Console.WriteLine("\nСоздаем и заполняем массив данными:");
+            myArray.FillArray();
+            myArray.Print();
+            Console.WriteLine("\nСортируем массив:");
+            myArray.Sort();
+            myArray.Print();
+            Console.Write("\nВведите элемен для поиска: ");
+            int.TryParse(Console.ReadLine(), out int numToSearch);
+            int index = myArray.BinarySearch(numToSearch);
+            Console.WriteLine("\nДанный элемент " + ((index == -1) ? "не найден" : $"найден по индексу: {index}"));
+
+            //////////////////////////////////////////
+            // Асимптотическая сложность равна log2 N
+            //////////////////////////////////////////
+        }
+    }
+}
