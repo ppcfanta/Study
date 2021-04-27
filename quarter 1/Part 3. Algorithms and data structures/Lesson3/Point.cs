9a1de419cf9800333728bfd3c47ce6ba8b182e55
Ivan Malakhov <artpromstroi@gmail.com>
2021-04-27 18:20:10 +0300

    homework q1.p3.lesson3 added

---
+++ quarter 1/Part 3. Algorithms and data structures/Lesson3/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson3
+{
+    public class PointClass
+    {
+        public float X;
+        public float Y;
+    }
+
+    public struct PointStruct
+    {
+        public float X;
+        public float Y;
+    }
+
+    public struct PointStructDouble
+    {
+        public double X;
+        public double Y;
+    }
+
+}
